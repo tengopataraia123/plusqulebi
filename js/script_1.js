0e44112719c4a3b296aab9abc03bb4dfee324104
Tengo Pataraia <pataraiamistera@gmail.com>
2025-09-05 03:36:09 -0700

    df

--- js/script_1.js
+++ js/script_1.js
@@ -51,7 +51,7 @@ copyId2.addEventListener("click", () => {
 
 // mock trades
 var trades = []
-fetch("http://165.227.142.232/lastTrans.json")
+fetch("http://plusqulebi.ge/lastTrans.json")
   .then(response => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
