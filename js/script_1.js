6bc423026a0626cea1949d6ec88d86422599fdc9
user <user@DESKTOP-QP00TH0>
2026-07-02 12:54:43 +0400

    update

--- js/script_1.js
+++ js/script_1.js
@@ -1,4 +1,4 @@
-const rate = { plus: 400, gel: 1.1 }; // editable baseline
+const rate = { plus: 400, gel: 1.3 }; // editable baseline
 const rateText = document.getElementById("rateText");
 const plusAmount = document.getElementById("plusAmount");
 const gelAmount = document.getElementById("gelAmount");
